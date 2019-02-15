bf8e072622c7bea1b24f3d54f90778dfed3e19cf
virginia_perozim@yahoo.com.br <virginia_perozim@yahoo.com.br>
2019-02-15 10:02:14 -0200

    Versão 1

---
+++ TesteForm/TesteForm/CompraVendaMoeda.cpp
@@ -0,0 +1,15 @@
+#include "CompraVendaMoeda.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace System::Text::RegularExpressions;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TesteForm::MyForm form;
+	Application::Run(%form);
+}
+
